5b81224b8b7bbc3e859b9232d1c4f34e4c49c174
jqmtony <jqmtony@PC-01.lan>
2018-03-27 00:31:46 +0800

    初始化导入

---
+++ src/main/java/org/xplus/sample/XplusSbSampleBootstrapApplication.java
@@ -0,0 +1,12 @@
+package org.xplus.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XplusSbSampleBootstrapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XplusSbSampleBootstrapApplication.class, args);
+	}
+}
